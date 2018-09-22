c860e7b8f18f2ebbfcf8c5895c05fce7509325f9
Rezza <rezza.primasta@telogic.com.sg>
2018-09-22 17:07:10 +0700

    code changed

--- AP01/mainWindow.py
+++ AP01/mainWindow.py
@@ -1,20 +1,30 @@
 import sys
+import inspect
 from PyQt4 import QtCore, QtGui, uic
 qtCreatorFile = "./mainwindow.ui"
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
 
-class myWidget(QtGui.QMainWindow, Ui_MainWindow):
+class mainWindow(QtGui.QMainWindow, Ui_MainWindow):
 	def __init__(self):
-		QtGui.QMainWindow.__init__(self)
-		Ui_MainWindow.__init__(self)
+		super(mainWindow,self).__init__()
+		uic.loadUi('./mainwindow.ui',self)
+		#QtGui.QMainWindow.__init__(self)
+		#Ui_MainWindow.__init__(self)
 		self.setupUi(self)
-
-def b1_clicked():
-   print "Button 1 clicked"
+		#QPushButton("pushButton_3")
+		self.pushButton_3.clicked.connect(self.btn3_clicked)
+		
+	def btn3_clicked(self):
+		print "Button 3 clicked"
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
-	window = myWidget()
+	window = mainWindow()
+	for name, data in inspect.getmembers(window):
+    		if name.startswith('__'):
+        		continue
+    		print('{} : {!r}'.format(name, data))
+	#window.button.clicked.connect(window.b1_clicked)
 	window.show()
 	sys.exit(app.exec_())

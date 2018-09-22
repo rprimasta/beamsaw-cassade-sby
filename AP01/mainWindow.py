07dd5374db1c5e1b89576841d40e0d2e5a931a65
Rezza <rezza.primasta@telogic.com.sg>
2018-09-22 18:06:26 +0700

    Open file NC done

--- AP01/mainWindow.py
+++ AP01/mainWindow.py
@@ -1,9 +1,18 @@
 import sys
 import inspect
-from PyQt4 import QtCore, QtGui, uic
+from PyQt4 import *
+from PyQt4 import uic,QtGui
+from PyQt4.QtGui import *
 qtCreatorFile = "./mainwindow.ui"
 
 Ui_MainWindow, QtBaseClass = uic.loadUiType(qtCreatorFile)
+
+def getClassMember(object):
+	for name, data in inspect.getmembers(object):
+    		if name.startswith('__'):
+        		continue
+    		print('{} : {!r}'.format(name, data))
+
 
 class mainWindow(QtGui.QMainWindow, Ui_MainWindow):
 	def __init__(self):
@@ -14,17 +23,29 @@ class mainWindow(QtGui.QMainWindow, Ui_MainWindow):
 		self.setupUi(self)
 		#QPushButton("pushButton_3")
 		self.pushButton_3.clicked.connect(self.btn3_clicked)
-		
+		self.pushButton_4.clicked.connect(self.btn4_clicked)
+		self.pushButton_5.clicked.connect(self.btn5_clicked)	
+		self.pushButton_19.clicked.connect(self.btn19_clicked)	
+		self.horizontalSlider_2.valueChanged.connect(self.feed_slider_moved)
+	def feed_slider_moved(self,pos):
+		self.label_19.setText('F : 5400(%d)' % pos)
+	def btn19_clicked(self):
+		w = QWidget()
+		w.resize(320, 240)
+		w.setWindowTitle("OPEN NC PROGRAM")
+		filename = QFileDialog.getOpenFileName(w, 'Open File', '/')
+		print filename
 	def btn3_clicked(self):
 		print "Button 3 clicked"
+		getClassMember(self.horizontalSlider_2)
+	def btn4_clicked(self):
+		self.stackedWidget.setCurrentIndex(1)
+	def btn5_clicked(self):
+		self.stackedWidget.setCurrentIndex(0)
 
 if __name__ == '__main__':
 	app = QtGui.QApplication(sys.argv)
 	window = mainWindow()
-	for name, data in inspect.getmembers(window):
-    		if name.startswith('__'):
-        		continue
-    		print('{} : {!r}'.format(name, data))
 	#window.button.clicked.connect(window.b1_clicked)
 	window.show()
 	sys.exit(app.exec_())
